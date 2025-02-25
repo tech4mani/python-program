7a8a8e684d4d9c80ccc1aad04fc7ca716275168a
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-02-25 15:34:29 +0530

    Add files via upload

---
+++ DICTIONARY METHOD.py
@@ -0,0 +1,29 @@
+#get()
+a={"mani":100,"siva":90,"arun":80}
+b=a.get("arun")
+print(b)
+
+#keys()
+a={"mani":100,"siva":90,"arun":80}
+b=a.keys()
+print(b)
+
+# #value
+a={"mani":100,"siva":90,"arun":80}
+b=a.values()
+print(b)
+
+# #iteams()
+a={"mani":100,"siva":90,"arun":80}
+b=a.items()
+print(b)
+
+#pop()
+a={"mani":100,"siva":90,"arun":80}
+b=a.pop("mani")
+print(b)
+
+#update()
+a={"mani":100,"siva":90,"arun":80}
+a.update({"hari":70})
+print(a)
