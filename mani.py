3f1408b334d26367a176da370cad130acf835837
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-02-25 14:35:07 +0530

    Add files via upload

---
+++ mani.py
@@ -0,0 +1,344 @@
+# number=123
+# name="mani"
+# print("number:",number)
+# print("name:",name)
+
+# name="mani"
+# age=21
+# print("my name is",name)
+# print("i am ",age,"old")
+
+# a=22
+# b=6
+# c=2023
+# print(a,"-",b,"-",c)
+
+# ab=1212
+# c=ab
+# print(c)
+
+# a=input("enter a no:")
+# b=input("enter a no:")
+# c=a+b
+# print(c)
+
+# a=int(input("enter a number:"))
+# b=int(input("enter a number:"))
+# print(a*100+b)
+
+# a=input("enter a no:")
+# b=input("enter a no:")
+# c=input("enter a no:")
+# d=a+b+c
+# print(d)
+
+# a=[1,2,3,4,5]
+# a.pop()
+# print(a)
+
+# a=[1,2,3,4,5]
+# a.remove(4)
+# print(a)
+
+# a=[1,2,3,4,5]
+# a.reverse()
+# print(a)
+
+# a=[1,2,3,4,5]
+# a.sort()
+# print(a)
+
+# a=int(input("enter a no:"))
+# b=int(input("enter a no:"))
+# c=int(input("enter a no:"))
+# print(a+b+c)
+
+
+# a=(input("enter a no:"))
+# b=(input("enter a no:"))
+# c=(input("enter a no:"))
+# print(a+b+c)
+
+# a=[1,2,3,4,5]
+# b=tuple(a)
+# print(b)
+
+
+# a=float(input("enter a no:"))
+# b=float(input("enter a no:"))
+# c=int(a+b)
+# print(c)
+
+# a=3.4
+# b=3.4
+# c=int(a+b)
+# print(c)
+
+# a=set(input("enter a no:",))
+# sum(("10"))
+# print(a)
+
+# a={10,20,30}
+# print(sum(a))
+
+# a=list(input("enter a no:"))
+# a.sort()
+# print(a)
+# print(sum(a))
+
+
+# a=(3.4,3.4)
+# x=list(a)
+# # a.append(4.5)
+# print(x)
+# x.append(4.5)
+# print(x)
+# x=sum(a)
+# # y=int(x)
+# # print(y)
+# a.append(4.3)
+# print(a)
+                   
+                                                                            
+# a.strip()
+# print(a[0:1],a[2:3],a[4:5],a[6:7],a[7:8],a[9:10])
+
+# print(a.strip())
+
+
+# a="h 'e' l 'l' o 'w' o 'r l d"
+# print(a[::2])
+
+# a="hello world"
+# b=a[::3]
+# print(b)
+
+# a="hello world"
+# b=a.replace("e","",1)
+# c=b.replace("","",3)
+# d=c.replace(" ","",5)
+# e=d.replace("o","",7)
+# f=e.replace("","",9)
+# print(f)
+
+# a=["hello world"]
+# b=
+
+#UPPER()
+# a="PaLani"
+# # print(a.lower())
+
+#LOWER()
+# a="PaLani"
+# print(a.upper ())
+
+#TITLE()
+# a="PaLani"
+# print(a.title())
+
+#REPLACE:
+# a="mani"
+# b=a.replace("mani","siva")
+# print(b)
+
+#SPLIT()
+# a="hello world this is mine"
+# b=a.split()
+# print(b)
+
+#list method
+#append(iteam)
+# a=[1,2,3,4,5,6]
+# a.append(7)
+# print(a)
+
+#EXTEND
+# a=[1,2,3,4,5,6]
+# a.extend([7,8,9])
+# print(a)
+
+#INSERT
+# a=["mani","siva","arun"]
+# a.insert(2,"hari")
+# print(a)
+
+#REMOVE
+# a=["mani","siva","arun"]
+# a.remove("arun")
+# print(a)
+
+#COPY
+# a=["mani","siva","arun"]
+# a.copy()
+# print(a)
+
+#SORT()
+# a=["mani","siva","arun"]
+# a.sort()
+# print(a)
+
+#REVERSE()
+# a=["mani","siva","arun"]
+# a.reverse()
+# print(a)
+
+#DICTIONARY METHOD
+#get()
+# a={"mani":100,"siva":90,"arun":80}
+# b=a.get("arun")
+# print(b)
+
+#keys()
+# a={"mani":100,"siva":90,"arun":80}
+# b=a.keys()
+# print(b)
+
+# #value
+# a={"mani":100,"siva":90,"arun":80}
+# b=a.values()
+# print(b)
+
+# #iteams()
+# a={"mani":100,"siva":90,"arun":80}
+# b=a.items()
+# print(b)
+
+#pop()
+# a={"mani":100,"siva":90,"arun":80}
+# b=a.pop("mani")
+# print(b)
+
+#update()
+# a={"mani":100,"siva":90,"arun":80}
+# a.update({"hari":70})
+# print(a)
+
+#SET METHOD
+# #ADD()
+# a={"mani","siva","arun"}
+# a.add("kavi")
+# print(a)
+
+#remove
+# a={"mani","siva","arun"}
+# a.remove("arun")
+# print(a)
+
+#discard()
+# a={"mani","siva","arun"}
+# a.discard("arun")
+# print(a)
+
+#union()
+# a={"mani","siva","arun"}
+# b={"hari","raj"}
+# c=a.union(b)
+# print(c)
+
+# #insertion()
+# a=[12,8,7,6,3]
+# a.insert(3,11)
+# print(a)
+
+# a=5
+# b=6
+# c=7
+# if a >b and a>c:
+#     print("a is largest")
+# elif b>c and b>a:
+#     print("b is largest")
+# else:
+#     print("c is largest")
+
+# mani="vote"
+# if (mani == "vote"):
+#     print("eligible to vote")
+# else:
+#     print("not eligible to vote")
+
+#even or not
+# a=10
+# if a%2 == 0:
+#     print("even no")
+# else:
+#     print("not even no")  
+
+# a=int(input("enter a no a:"))
+# b=int(input("enter a no b:"))
+# c=int(input("enter a no c:"))
+# d=int(input("enter a no d:"))
+# if 85 <= a <= 100:
+#     print("A Grade")
+# if 60 <= b <= 84:
+#     print("B Grade")
+# if 40 <= c <= 59:
+#     print("c Grade")
+# if 35 <= d <= 39:
+#     print("d Grade")
+# else:
+#     print("fail")
+
+# a=int(input("enter a no a:"))
+# b=int(input("enter a no b:"))
+# c=int(input("enter a no c:"))
+# d=int(input("enter a no d:"))
+# if 85 <= a <= 100:
+#     print("A Grade")
+# elif 60 <= b <= 84:
+#     print("B Grade")
+# elif 40 <= c <= 59:
+#     print("c Grade")
+# elif 35 <= d <= 39:
+#     print("d Grade")
+# else:
+#     print("fail")
+    
+# a=input("enter a name:")
+# if "w" in a:
+#     print(" is present in hello world")
+# else:
+#     print(" is not present in hello world")
+
+# a=int(input("enter a year:"))
+# b=a % 4==0
+# if b:
+#     print("is a leap year")
+# else: 
+#     print("is not a leap year")
+
+# a="mani"
+# for word in a:
+#     print(f"{word}")
+
+a=[1,2,3,4,5,6,7,8,9,10]
+# b=6
+for num in a:
+    # if num ==b:
+        # break
+        print(f"{num}")
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# for num in a:
+#       print(f"{a%2==1,}")
+# for i in range(1,11):
+#     print(f":{i%2==1,}")
+
+
+
+for i in range(1,11):
+  if(i%2==0):
+    print("even:",i)
+  else:    
+    print("odd:",i)
+    
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# for num in a:
+#     print(f"{num}")
+#     if (a%2==0): 
+#         print()
+#     else:
+#         print(a)
+    
+# a=[1,2,3,4]
+# for a in range
