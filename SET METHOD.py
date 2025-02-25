d850d2ecee88febc2adec88d931a975c7de812f8
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-02-25 15:36:56 +0530

    Add files via upload

---
+++ SET METHOD.py
@@ -0,0 +1,25 @@
+# #ADD()
+a={"mani","siva","arun"}
+a.add("kavi")
+print(a)
+
+#remove
+a={"mani","siva","arun"}
+a.remove("arun")
+print(a)
+
+#discard()
+a={"mani","siva","arun"}
+a.discard("arun")
+print(a)
+
+#union()
+a={"mani","siva","arun"}
+b={"hari","raj"}
+c=a.union(b)
+print(c)
+
+# #insert()
+a=[12,8,7,6,3]
+a.insert(3,11)
+print(a)
