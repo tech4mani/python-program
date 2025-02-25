23335bc5e704dff75ff242cee999cf631179182a
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-02-25 15:01:32 +0530

    Add files via upload

---
+++ print statement.py
@@ -0,0 +1,33 @@
+number=123
+name="mani"
+print("number:",number)
+print("name:",name)
+
+name="mani"
+age=21
+print("my name is",name)
+print("i am ",age,"old")
+
+a=22
+b=6
+c=2023
+print(a,"-",b,"-",c)
+
+ab=1212
+c=ab
+print(c)
+
+a=input("enter a no:")
+b=input("enter a no:")
+c=a+b
+print(c)
+
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+print(a*100+b)
+
+a=input("enter a no:")
+b=input("enter a no:")
+c=input("enter a no:")
+d=a+b+c
+print(d)
