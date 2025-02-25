0ea16a5e4e6a31fcf9c5962049a2f2cde60b2168
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-02-25 15:16:22 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,23 @@
+a=[1,2,3,4,5]
+b=tuple(a)
+print(b)
+
+
+a=float(input("enter a no:"))
+b=float(input("enter a no:"))
+c=int(a+b)
+print(c)
+
+a=3.4
+b=3.4
+c=int(a+b)
+print(c)
+
+a={10,20,30}
+print(sum(a))
+
+a=(3.4,3.4)
+x=list(a)
+x.append(4.5)
+print(x)
+x=sum(a)
