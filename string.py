a7a2bc72758488ab830d224f361a67c737ef558f
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-02-25 15:30:57 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+#UPPER()
+a="PaLani"
+# print(a.lower())
+
+#LOWER()
+a="PaLani"
+print(a.upper ())
+
+#TITLE()
+a="PaLani"
+print(a.title())
+
+#find()
+a="PaLani is my friend"
+b=a.find("is")
+print(b)
+
+#REPLACE:
+a="mani"
+b=a.replace("mani","siva")
+print(b)
+
+#SPLIT()
+a="hello world this is mine"
+b=a.split()
+print(b)
