7af614f22eb5af1bfbb3a5d471018a91dd479c9e
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-02-25 15:32:21 +0530

    Add files via upload

---
+++ list method.py
@@ -0,0 +1,34 @@
+#append(iteam)
+# a=[1,2,3,4,5,6]
+# a.append(7)
+# print(a)
+
+#EXTEND
+# a=[1,2,3,4,5,6]
+# a.extend([7,8,9])
+# print(a)
+
+#INSERT
+# a=["mani","siva","arun"]
+# a.insert(2,"hari")
+# print(a)
+
+#REMOVE
+# a=["mani","siva","arun"]
+# a.remove("arun")
+# print(a)
+
+#COPY
+# a=["mani","siva","arun"]
+# a.copy()
+# print(a)
+
+#SORT()
+# a=["mani","siva","arun"]
+# a.sort()
+# print(a)
+
+#REVERSE()
+# a=["mani","siva","arun"]
+# a.reverse()
+# print(a)
